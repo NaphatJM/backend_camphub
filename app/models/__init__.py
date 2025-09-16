d611e957a75ab4cfd19ce72dd5ca746c9c2e3794
6510110004 <6510110004@email.psu.ac.th>
2025-09-16 11:27:23 +0700

    feat: Add cousre_schedule_model , Add status field in enrollment & relation CourseSchedule with  Couse

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,16 +3,18 @@ from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from app.core.config import get_settings
 
+# Models
 from .user_model import User
 from .faculty_model import Faculty
 from .role_model import Role
 from .course_model import Course
 from .enrollment_model import Enrollment
 from .course_teacher_link import CourseTeacherLink
+from .course_schedule_model import CourseSchedule
 
 settings = get_settings()
 
-# ✅ ใช้ async engine
+# ✅ Async engine
 async_engine = create_async_engine(
     settings.SQLDB_URL,
     echo=False,
@@ -20,20 +22,28 @@ async_engine = create_async_engine(
 )
 
 
+# Initialize all tables
 async def init_db():
     async with async_engine.begin() as conn:
+        # สร้างทุก table จาก metadata ของ SQLModel
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
+# Async session generator
 async def get_session() -> AsyncIterator[AsyncSession]:
     async with AsyncSession(async_engine) as session:
         yield session
 
 
+# __all__ สำหรับ import แบบ *
 __all__ = [
     "User",
     "Faculty",
     "Role",
+    "Course",
+    "CourseSchedule",
+    "Enrollment",
+    "CourseTeacherLink",
     "init_db",
     "get_session",
     "async_engine",
---
+++ app/models/course_schedule_model.py
@@ -0,0 +1,21 @@
+from typing import Optional, TYPE_CHECKING
+from datetime import time
+from sqlmodel import SQLModel, Field, Relationship
+
+if TYPE_CHECKING:
+    from app.models.course_model import Course
+
+
+class CourseSchedule(SQLModel, table=True):
+    __tablename__ = "course_schedule"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    course_id: int = Field(foreign_key="course.id")  # FK ไปยัง Course
+
+    day_of_week: str = Field(max_length=20)  # เช่น "Monday", "Tuesday"
+    start_time: time
+    end_time: time
+    room: str = Field(max_length=50)
+
+    # Relationship กลับไปที่ Course
+    course: Optional["Course"] = Relationship(back_populates="schedules")
