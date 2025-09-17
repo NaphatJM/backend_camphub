4d537f594939ae8f369dd170d0b5c2b1db875386
6510110004 <6510110004@email.psu.ac.th>
2025-09-17 10:48:08 +0700

    feat: preload relation role and faculty when user login & fix call invalid function

--- app/core/deps.py
+++ app/core/deps.py
@@ -2,6 +2,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 from app.models import get_session, User
 from app.core.security import decode_token
 
@@ -21,13 +22,22 @@ async def get_current_user(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
             )
 
-        result = await session.execute(select(User).where(User.username == username))
+        result = await session.execute(
+            select(User)
+            .options(
+                selectinload(User.role),
+                selectinload(User.faculty),
+            )
+            .where(User.username == username)
+        )
         user = result.scalar_one_or_none()
+
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
             )
         return user
+
     except ValueError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token"
